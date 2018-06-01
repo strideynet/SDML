1098b473c25163180df16b3d62a82a053ade0596
Unknown <noah.stride@selfsown.com>
2018-06-01 16:41:25 +0100

    Config

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+mongoose.connect('mongodb://localhost/sdml').then(() => {
+  console.log('Database connected successfully')
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
+
 app.use(bodyParser.json())
 
 app.use('/api', require('./router'))
